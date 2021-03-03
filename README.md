6c1686f5a7d98a528765c2c9d3e10d3e4df78110
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-03-03 13:08:58 +0200

    add to more guess

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# CodeBase
-my journey
+# Guessing Game Website!
+
+![](https://i.ytimg.com/vi/li_RdvAKIfw/maxresdefault.jpg)
+
+
+> ## I Have Created This Game Using: HTML, CSS, JS.
+
+* I have added **7 Questions** About Me.
+
+* Questions that I previously added have used with it (IF Statement, Switch Statement, While loop, foor loop, Array) With 1 Function.
+
+> I hope you like the game guys..
--- js/app.js
+++ js/app.js
@@ -1,108 +1,150 @@
 'use strict';
 let pointer = 0;
 
-alert('Hey! ,Welcome to My website');
+alert('Hey EveryOne!, Welcome to My website guy\'s *-*!');
+let userName;
 
-let userName = prompt('What\'s your name?', 'Enter your name Here..');
-alert('Welcome to my website ' + userName);
+function PromptStudentName() {
+    userName = prompt('Enter Your Name Please..', ' you can Enter your name Here..');
+    if (isNaN(userName) === false) {
+        alert('You need to write a correct name! ');
+        PromptStudentName();
+    } else if (userName == "'" || userName == "`" || userName == "!" || userName == "@" || userName == "#" || userName == "$" || userName == "%" || userName == "^" || userName == "&" || userName == "*" || userName == "(" || userName == ")" || userName == "-" || userName == "_" || userName == "+" || userName == "=" || userName == "/" || userName == "~" || userName == "<" || userName == ">" || userName == "," || userName == ";" || userName == ":" || userName == "|" || userName == "?" || userName == "{" || userName == "}" || userName == "[" || userName == "]" || userName == "¬" || userName == "£" || userName == ' " ' || userName == "\\") {
+        alert("You need to write a correct name without symbol!");
+        PromptStudentName();
+    }
+    return userName;
+}
+PromptStudentName();
 
-// console.log('Welcome to my website ' + userName);
+alert('Welcome To My Website ' + userName + '!');
 
-let g = prompt('how was lecture today ? ', 'choose:[confused me a bit /good so far /rather complicated /was useful/]').toLowerCase();
+// console.log('Welcome to my website ' + userName + '!');
 
-if (g === 'good so far' || g === 'was useful') {
-    alert('thats cool ..');
+let answer = prompt('If I were you, would you expect me to want to play a guessing game with you ' + userName + ' ?', '(Yes-y)/(No-n)').toLowerCase();
 
-} else if (g === 'confused me a bit' || g === 'rather complicated') {
-    alert('you need just repeat watch recording lecture one more and i think you will be good!');
-
-} else {
-    alert('Sorry I can\'t get your answer');
+if (answer === 'yes' || answer === 'y') {
+    alert('Yeah ' + userName + ' of course i want that, why not!');
+}
+else if (answer === 'no' || answer === 'n') {
+    alert('Ooh! Why ' + userName + ' ? Actually i want in fact *_*, let\'s beginning!');
+}
+else {
+    alert('Try Again! MR. ' + userName);
 }
 
-function guess1() {
-    let user = prompt('If I were you, would you expect me to want to play a guessing game with you man?', '(Yes-y)/(No-n)').toLowerCase();
 
-    if (user === 'yes' || user === 'y') {
-        alert('Yeah ' + userName + ' of course i want that, why not!');
+let lecture = prompt('How Was Lecture Today ' + userName + ' ?', 'choose:[confused me a bit /good so far /rather complicated /was useful/]').toLowerCase();
+
+while (lecture === 'good so far' || lecture === 'was useful') {
+    alert('That\'s Cool ' + userName + '..');
+    pointer++;
+    break;
+}
+while (lecture === 'confused me a bit' || lecture === 'rather complicated') {
+    alert('Don\'t Worry!, just You need just repeat watch recording lecture one more and i think you will be good inshaallah!');
+    break;
+}
+
+// console.log('1:  ' + pointer);
+
+let math = prompt('Guess, do you think I hate Mathmatic MR. ' + userName + ' ?', '(Yes-y)/(No-n)').toLowerCase();
+switch (true) {
+    case (math === 'yes' || math === 'y'):
+        alert('OH YEAH! Really i hate Math');
         pointer++;
-    }
-    else if (user === 'no' || user === 'n') {
-        alert('Oops! Why man , i want in fact *_*');
-    }
-    else alert('Try Again!');
+        break;
+    case (math === 'no' || math === 'n'):
+        alert('You are wrong , i don\'t like math ever never Ya ' + userName + '!');
+        break;
+    default: alert('wrong input! MR. ' + userName);
 }
-guess1();
 
-function guess2() {
-    let user = prompt(' Guess, do you think I hate Mathmatic???', '(Yes-y)/(No-n)').toLowerCase();
-    switch (true) {
-        case (user === 'yes' || user === 'y'):
-            alert('OH YEAH! Really i hate math');
-            pointer++;
-            break;
-        case (user === 'no' || user === 'n'):
-            alert('You are wrong , i don\'t like math ever never!');
-            break;
-        default: alert('wrong input!');
-    }
+// console.log('2:  ' + pointer);
+
+let userSm = prompt('Do you think i like social media?', '(Yes-y)/(No-n)').toLowerCase();
+if (userSm === 'yes' || userSm === 'y') {
+    alert('yes, I like it ' + userName + '.');
+    pointer++;
 }
-guess2();
+else if (userSm === 'no' || userSm === 'n') {
+    alert('Oo!, Uncoreect ' + userName + '!');
+}
+else alert('What are u talking About ' + userName + '!');
 
-function guess3() {
-    let user = prompt('Do you think i like social media?', '(Yes-y)/(No-n)').toLowerCase();
-    if (user === 'yes' || user === 'y') {
-        alert('yes, I like it ' + userName + '.');
+// console.log('3:  ' + pointer);
+
+let dream = prompt('Can you guess what country I dream to live in ' + userName + ' ?', 'choose:[USA /USE /Canada/]').toLowerCase();
+
+while (dream === 'USA' || dream == 'usa') {
+    alert('That\'s Correct ' + userName + '.. Really this is my dream!');
+    pointer++;
+    break;
+}
+while (dream === 'UAE' || dream === 'uae' || dream === 'Canada' || dream === 'canada') {
+    alert('Uncorrect! ' + userName);
+    break;
+}
+
+// console.log('4:  ' + pointer);
+
+let favTeam = prompt('Do you think i like FCBarcelona Team *_*?', '(Yes-y)/(No-n)').toLowerCase();
+if (favTeam === 'yes' || favTeam === 'y') {
+    alert('yes, I am a fan of the Barcelona team, and I am fanatic about it ' + userName + '.');
+    pointer++;
+}
+else if (favTeam === 'no' || favTeam === 'n') {
+    alert('Oo!, Uncoreect ' + userName + ', Barcelona is My Fav Team!');
+}
+
+// console.log('5:  ' + pointer);
+
+let userReview = prompt('Are my questions complex and boring and so on? ', '(Yes-y)/(No-n)').toLowerCase();
+
+while (isNaN(userReview) === false) {
+    alert('Please enter a valid number');
+    userReview = prompt('Are my questions complex and boring and so on? ', '(Yes-y)/(No-n)').toLowerCase();
+}
+if (userReview === 'yes' || userReview === 'y') {
+    alert('Really! Why MR. ' + userName + ' ?');
+
+} else if (userReview === 'no' || userReview === 'n') {
+    alert('Thank you MR.' + userName + '.');
+    pointer++;
+}
+
+// console.log('6:  ' + pointer);
+
+let statusOfnum = false;
+let userAvg;
+let holder = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let convert = holder.values.toString();
+for (let z = 0; z <= 5; z++) {
+    // userAvg = Number(prompt('guess the number in my mind From 1-10', holder));
+    userAvg = prompt('guess the number in my mind From 1-10', holder);
+    if (isNaN(userAvg)) {
+        alert('Please enter a valid number');
+    }
+    else if (userAvg == holder[4]) { //else if (userAvg === holder[4])
+        alert('Correct Answer, Great Job ' + userName + '!');
         pointer++;
+        statusOfnum = true;
+        break;
     }
-    else if (user === 'no' || user === 'n') {
-        alert('Oo! , Uncoreect!');
+    else if (userAvg > holder[4]) {
+        alert('too high');
     }
-    else alert('??!');
-}
-guess3();
-
-function guess4() {
-    let user = prompt('Are my questions complex and boring and so on? ? ', '(Yes-y)/(No-n)').toLowerCase();
-
-    if (user === 'yes' || user === 'y') {
-        alert('Really! Why! MR. ' + userName);
-    } else if (user === 'no' || user === 'n') {
-        alert('Thank you MR.' + userName);
-        pointer++;
-    }
-    else alert('??!');
-}
-guess4();
-
-let numcorrect = false;
-let mynum = 5;
-function guess5(myNumber, numberCorrect) {
-    for (let h = 0; h < 4; h++) {
-        let usernum = Number(prompt('guess the number in my mind From 1-10'));
-
-        if (usernum === myNumber) {
-            alert('Correct answer');
-            pointer++;
-            numberCorrect = true;
-            break;
-        }
-        else if (usernum > myNumber) {
-            alert('too high');
-        }
-        else if (usernum < myNumber) {
-            alert('too low');
-        }
-        else ('Bad input');
-    }
-    if (!numberCorrect) {
-        alert('the correct number is: ' + mynum);
+    else if (userAvg < holder[4]) {
+        alert('too low');
     }
 }
-guess5(mynum, numcorrect);
+
+if (!statusOfnum) {
+    alert('The Correct Answer is: ' + holder[4] + 'Good luck later On ' + userName + '!');
+}
+
+// console.log('7:  ' + pointer);
 
 let element = document.getElementById('n-html');
-element.innerText = 'Hey! MR. ' + userName + ", My Name Is Ahmad AlDabouqi";
-alert('Your Score is: ' + pointer + '/5');
-
-// console.log(pointer);
+element.innerText = 'Hey! MR. ' + userName + ', My Name Is Ahmad AlDabouqi';
+alert('Your Score is: ' + pointer + '/7');
